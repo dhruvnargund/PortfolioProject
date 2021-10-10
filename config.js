7c6ba6c5685a551392c232a3f71b0b2da92b87bf
dhruvnargund <nargund36@gmail.com>
2021-10-11 03:31:15 +0530

    added DB url so that anyone can test

--- config.js
+++
@@ -1,5 +0,0 @@
-const config = {
-    MONGO_URI = "mongodb+srv://harsha:reddy@cluster0.22slh.mongodb.net/DeakinSocial"
-}
-
-module.exports = config;
--- server/database/database.js
+++ server/database/database.js
@@ -1,14 +1,12 @@
 const mongoose = require('mongoose');
-const config = require('../../config');
+// const config = require('../../config');
 
 const Connect = async () => {
     try {
         // Mongodb connection
-        const con = await mongoose.connect(config.MONGO_URI, {
+        const con = await mongoose.connect("mongodb+srv://dhruv:dhruv@uploader.9dvp6.mongodb.net/Uploader?retryWrites=true&w=majority", {
             useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false,
-            useCreateIndex: true
+            useUnifiedTopology: true
         })
 
         console.log(`MongoDB Connected : ${con.connection.host}`);
